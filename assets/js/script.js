b965191ac8c6d6ac4be0f6706d014433f0b43823
lreshmaprdxn <reshmal@prdxn.com>
2020-03-20 18:32:59 +0530

    blogs slider function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,7 +153,42 @@ $(document).ready (function() {
 
 
 
-
+// slider function
+$(document).ready (function() {
+	$('.blogs-list-slider').slick ({
+		dots:true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+		// Responsive carousel
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				infinite: false,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});  
+});
 
 
 
