69a217a3298ba78e2edcc892a99425d34191a7f2
lreshmaprdxn <reshmal@prdxn.com>
2020-03-20 17:54:16 +0530

    navbar function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-/* Author: 
+// Function for navigation
+var hamburger = document.querySelector ('.hamburger');
+var nav = document.querySelector ('.menu');
+hamburger.addEventListener ('click',navShow);
 
-*/
+function navShow() {
+  nav.classList.toggle('show');  
+  hamburger.classList.toggle('open');
+  document.querySelector('html').classList.toggle('no-scroll');
+}
 
 
 
